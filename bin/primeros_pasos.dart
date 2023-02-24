e3de6a55634660ff9d236fefd856cef1a50a8caa
Alex <ronaldalexandervacca@unicesar.edu.co>
2023-02-24 13:46:48 -0500

    Map y Listas

--- bin/primeros_pasos.dart
+++ bin/primeros_pasos.dart
@@ -19,8 +19,120 @@ void main() {
   */
   // concatenar();
 
-  menuA(10);
-  menuB(10);
+  //menuA(10);
+  //menuB(10);
+
+  //listas();
+  /*
+  int cod = 1001;
+  String nombre = "Alexillo el Gordillo";
+  int creditos = 5;
+  List<String> materias = ['P Movil', 'BD', 'Arq Comp'];
+  bool estado = true;
+
+  List<dynamic> listar = [];
+  listar.add(listarEstudiantes(cod, nombre, creditos, materias, estado));
+  print(listar);
+  cod = 1002;
+  nombre = "Juan";
+  creditos = 5;
+  materias = ['P Movil', 'BD'];
+  estado = true;
+  listar.add(listarEstudiantes(cod, nombre, creditos, materias, estado));
+  print(listar);
+
+  print('-----------------------------');
+  print('----Lista de Estudiantes-----');
+  print('-----------------------------');
+
+  for (int k = 0; k < listar.length; k++) {
+    print("Nombre: ${listar[k][1]}");
+     for(int r=0; r<listar[k][3].length;r++) {
+       print("$r -> ${listar[k][3][r]}");
+     } 
+  }
+  */
+  datosMap();
+}
+
+void datosMap() {
+  var datos = {"id": 1001, "nombre": "alex", "prof": "Inge"};
+
+  print(datos);
+
+  print(datos["id"]);
+  datos.addAll({"edad": 23});
+  print(datos);
+
+  datos["id"] = 1002;
+  print(datos);
+
+  datos["sexo"] = "M";
+  print(datos);
+
+  Map<int, String> vocales = {1: "A", 2: "E", 3: "I", 4: "O"};
+
+  Map<String, dynamic> est1 = {
+    "id": 1001,
+    "nombre": "Alexillo",
+    "creditos": 3,
+    "materias": ["P Movil", "BD", "Inge sift"],
+    "ubicacion": {"lo": 123, "la": 123}
+  };
+
+  List<Map<String, dynamic>> listadoEst = [];
+  listadoEst.add(est1);
+  listadoEst.add(est1);
+  listadoEst.add(est1);
+  print(listadoEst);
+
+  print(listadoEst[0]["nombre"]);
+}
+
+List<dynamic> listarEstudiantes(
+    int c, String n, int cx, List<String> m, bool e) {
+  List<dynamic> est = [];
+  est.addAll([c, n, cx, m, e]);
+  return est;
+}
+
+void listas() {
+  List<int> pares = [2, 4, 6, 4, 8, 10];
+  List<String> vocales = [
+    'A',
+    'E',
+  ];
+  List<dynamic> varios = [123, "Letras", true, [], 2.6];
+
+  print(pares);
+  pares.add(12);
+  print(pares);
+  pares.addAll([14, 16, 18, 20]);
+  print(pares);
+  print(pares[0]);
+  print(pares[pares.length - 1]);
+  print(pares.first);
+  print(pares.last);
+
+  pares.remove(20);
+  print(pares);
+  pares.removeAt(0);
+  print(pares);
+  pares.add(4);
+  print(pares);
+  pares.remove(4);
+  print(pares);
+
+  for (int i = 0; i < pares.length; i++) {
+    print("Lista($i): ${pares[i]}");
+    pares.remove(4);
+  }
+  for (var elem in pares) {
+    print("Lista: $elem");
+  }
+
+  pares[0] = 1001;
+  print(pares);
 }
 
 const selmenu = [
